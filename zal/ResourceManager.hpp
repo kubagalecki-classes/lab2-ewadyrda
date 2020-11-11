bac1dfc93d0b64d12edd73796189d8dea5bb96c5
ewadyrda <73639161+ewadyrda@users.noreply.github.com>
2020-11-11 17:13:03 +0100

    Update ResourceManager.hpp

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -1,8 +1,47 @@
 #pragma once
 
 #include "Resource.hpp"
-
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+   private:
+
+       Resource* wsk=nullptr;
+
+    public:
+
+        ResourceManager(){wsk=new Resource;}
+
+        ~ResourceManager(){delete wsk;}
+
+        ResourceManager(const ResourceManager& e){
+	          wsk=new Resource{*e.wsk};
+	          }
+
+         ResourceManager(ResourceManager&& e){
+            wsk=move(e.wsk);
+            e.wsk=nullptr;
+            }
+
+        ResourceManager& operator=(const ResourceManager& e){
+            if(!(wsk==nullptr)){
+                delete wsk;
+            }
+		          wsk=new Resource{*e.wsk};
+		          delete *this;
+        }
+
+        ResourceManager& operator=(const ResourceManager&& e){
+            if(&prze==this){
+                return *this;
+            }
+		        else if(!(wsk==nullptr){
+              delete wsk;
+		        }
+            wsk=move(e.wsk);
+            e.wsk=nullptr;
+            return *this;
+        }
+
+        double get() {return wsk->get();}
+
 };
